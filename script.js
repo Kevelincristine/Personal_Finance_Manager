a6153a463d8eefc5d02ebc704f6158d15ccad31e
Kevelin Cristine Santos <kevelincristine82@gmail.com>
2026-01-27 10:54:52 -0300

    Add transaction history and balance update logic
    Implemented transaction addition and history rendering in script.js, storing transactions and balance in localStorage. Updated index.html to add an 'Other...' category and refactored the history section to display a dynamic transaction list.

--- script.js
+++ script.js
@@ -38,4 +38,83 @@ window.addEventListener("load",()=>{
         document.querySelector("#user-name").textContent = savedName;
         document.querySelector("#balance").textContent = savedbalance;
     }
+  renderHistory();
 })
+
+const tranValue = document.querySelector("#transaction-value");
+const tranType = document.querySelector("#transaction-type");
+const tranCategory = document.querySelector("#transaction-category");
+const addBtn = document.querySelector("#add-transaction") ;
+let updatedBalance;
+
+
+
+addBtn.addEventListener("click",()=>{
+  const value = tranValue.value;
+  const type = tranType.value;
+  const category = tranCategory.value;
+  if ( value.trim() === "" || type.trim() === "" || category.trim()=== ""){
+    alert("type something first");
+    return;
+  } else {
+     const valueNumber = Number(value);
+     const currentBalance = Number(localStorage.getItem("balance") || 0);
+
+    if ( type === "income"){
+       updatedBalance = currentBalance + valueNumber;
+    } else {
+      updatedBalance = currentBalance - valueNumber;
+    }
+    localStorage.setItem("balance", updatedBalance);
+    balancespan.textContent = updatedBalance;
+
+
+   const now = new Date();
+   const date = now.toLocaleDateString("pt-BR");
+   const time= now.toLocaleTimeString("pt-BR", {
+    hour: "2-digit",
+    minute: "2-digit"
+   });
+
+   const transactions = JSON.parse(localStorage.getItem("transactions"))  || [];
+
+  const newTransaction = {
+    value: valueNumber,
+    type,
+    category,
+    date,
+    time
+  };
+  transactions.push(newTransaction);
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+  renderHistory();
+  }
+  
+  });
+
+  function renderHistory(){
+    const history = document.querySelector("#history");
+    history.innerHTML = "";
+
+    const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+    transactions.forEach(item => {
+      const li = document.createElement("li");
+      li.textContent =  `${item.date} ${item.time} | ${item.category} | ${item.type} | $ ${item.value}`;
+      history.appendChild(li);
+    });
+  }
+  function calculateBalance(){
+      const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+      let balance = 0;
+
+      transactions.forEach(item =>{
+        if (item.type === "income"){
+          balance += item.value;
+        } else {
+          balance -= item.value;
+        }
+      });
+       
+
+
+  }
